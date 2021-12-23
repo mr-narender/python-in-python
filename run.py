3dc7ea18d05ac0e61a14fa8bd52ba60c86a11209
liamsmith3194 <smith.liam1994@gmail.com>
2021-12-23 18:18:43 +0000

    snake position set

--- run.py
+++ run.py
@@ -3,9 +3,18 @@ import sys
 import random
 
 class Snake():
-    def __init__(self):
-        self.length = 1
-        self.positions = [((screen_width/2), (screen_height/2))]
-        self.direction = random.choice([up, down, left, right])
-        self.color = (17, 24, 47)
-        self.score = 0
+    def __init__(snake):
+        snake.length = 1
+        snake.positions = [((screen_width/2), (screen_height/2))]
+        snake.direction = random.choice([up, down, left, right])
+        snake.color = (48, 109, 223)
+        snake.score = 0
+
+    def get_head_position(snake):
+        return snake.positions[0]
+
+    def turn(snake, point):
+        if snake.length > 1 and (point[0]*-1, point[1]*-1) == snake.direction:
+            return
+        else:
+            snake.direction = point
