97469b8a811bedfc10eb59f2ba73ee4fa730a426
liamsmith3194 <smith.liam1994@gmail.com>
2021-12-27 16:55:04 +0000

    grid function added produces coloured squares based on if the grid number is disable by two or not

--- run.py
+++ run.py
@@ -21,6 +21,19 @@ down = (0,1)
 left = (-1,0)
 right = (1,0)
 
+def snake_grid(surface):
+  """
+  Creates the chequered grid for the game based on if the grid number is disable by two or not. If it is apply the darker colour if not, apply the lighter colour.
+  """
+  for y in range(0, int(grid_height)):
+    for x in range(0, int(grid_width)):
+      if (x+y)%2 == 0:
+          light_square = pygame.Rect((x*gridsize, y*gridsize), (gridsize,gridsize))
+          pygame.draw.rect(surface,(38,52,69), light_square)
+      else:
+        dark_square = pygame.Rect((x*gridsize, y*gridsize), (gridsize,gridsize))
+        pygame.draw.rect(surface, (32,41,55), dark_square)
+
 class Snake():
     def __init__(snake):
       """
@@ -33,7 +46,10 @@ class Snake():
       snake.score = 0
 
     def snake_direction(snake):
-        return snake.positions[0]
+      """
+      Front square denotes the head of the snake, 
+      """
+      return snake.position[0]
 
     def turn(snake, point):
         if snake.length > 1 and (point[0]*-1, point[1]*-1) == snake.direction:
