e12160f0dcb543b7e9a5af337c9da1d80ff43384
liamsmith3194 <smith.liam1994@gmail.com>
2022-01-03 18:24:33 +0000

    snake now moving on keyboard command, next step is to eat the apple and move to random location while increasing score and snake size

--- run.py
+++ run.py
@@ -1,51 +1,42 @@
 import pygame
 import sys
 import random
-
-# Title on game window
-pygame.display.set_caption('Python in Python')
-
-# Screen dimensions
+ 
+pygame.init()
+  
 screen_width = 400
 screen_height = 400
+ 
 game_board = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Python in Python')
+ 
+clock = pygame.time.Clock()
+
+# Colours
+white = (255, 255, 255)
+black = (0, 0, 0)
+lighter_square = (38, 52, 69)
+darker_square = (32, 41, 55)
 
 # Grid dimensions
 grid_size = 20
 # Produces a 20 x 20 grid
 grid_width = 20
 grid_height = 20
-
-# Using a Y and X axis, keydown actions create the below movements.
-up = (0, -1)
-right = (1, 0)
-down = (0, 1)
-left = (-1, 0)
-
-# Colours
-lighter_square = (38, 52, 69)
-darker_square = (32, 41, 55)
-white = (255, 255, 255)
-
+ 
 # Snake
 snake_position = (200, 200)
-snake_head_colour = (0, 159, 0)
-snake_body_colour = (0, 0, 159)
-snake_size = 1
+snake_colour = (0, 159, 0)
+snake_size = 20
 snake_score = 0
 snake_speed = 10
 
 # Apple
 apple_position = (200, 0)
 apple_colour = (200, 0, 0)
-
-# Game specific variables
-snake_x = 200
-snake_y = 200
-move_x = 0
-move_y = 0
-snake_size = 20
-fps = 10
+ 
+lost_font = pygame.font.SysFont("helvetica", 20)
+score_font = pygame.font.SysFont("Courier", 16)
 
 def background(game_board):
     """
@@ -61,105 +52,97 @@ def background(game_board):
                 square_two = pygame.Rect((x * grid_size, y * grid_size),
                                          (grid_size, grid_size))
                 pygame.draw.rect(game_board, (darker_square), square_two)
+ 
+def snake(snake_size, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(game_board, snake_colour, [x[0], x[1], snake_size, snake_size])
+        pygame.draw.rect(game_board, white, [x[0], x[1], snake_size, snake_size],1)
 
-def snake(game_board):
-    """
-  Creates the snake using the pygame.rect, loads the snake in the given position via the variable snake_position and coloured in snake_colour. A further white square has been used for a border.
-  """
-    snake_square = pygame.Rect((snake_position[0], snake_position[1]),
-                               (grid_size, grid_size))
-    pygame.draw.rect(game_board, snake_head_colour, [snake_x, snake_y, snake_size, snake_size])
-    pygame.draw.rect(game_board, (white), [snake_x, snake_y, snake_size, snake_size],1)
+def snake_score(score):
+    value = score_font.render("Score: " + str(score), True, white)
+    game_board.blit(value, [5, 10])
 
+def message(msg, color):
+    mesg = lost_font.render(msg, True, color)
+    game_board.blit(mesg, [100, screen_height / 2]) 
 
-def snake_direction(snake):
-    """
-  Front square denotes the head of the snake,
-  """
-    return snake.head[0]
+ 
+def gameloop():
+    game_over = False
+    game_end = False
+ 
+    snake_x = screen_width / 2
+    snake_y = screen_height / 2
+ 
+    move_horizontal = 0
+    move_vertical = 0
+ 
+    snake_list = []
+    snake_length = 1
+ 
+    apple_x = round(random.randrange(0, screen_width - snake_size) / grid_size) * grid_size
+    apple_y = round(random.randrange(0, screen_height - snake_size) / grid_size) * grid_size
+ 
+    while not game_over:
+ 
+        while game_end == True:
+            game_board.fill(black)
+            message("You lost!\n Press Enter to Play Again\n Press Q to Quit", white)
+            snake_score(snake_length - 1)
+            pygame.display.update()
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_end = False
+                    if event.key == pygame.K_RETURN:
+                        gameloop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    move_horizontal = -20
+                    move_vertical = 0
+                elif event.key == pygame.K_RIGHT:
+                    move_horizontal = 20
+                    move_vertical = 0
+                elif event.key == pygame.K_UP:
+                    move_vertical = -20
+                    move_horizontal = 0
+                elif event.key == pygame.K_DOWN:
+                    move_vertical = 20
+                    move_horizontal = 0
+ 
+        if snake_x >= screen_width or snake_x < 0 or snake_y >= screen_height or snake_y < 0:
+            game_end = True
+        snake_x += move_horizontal
+        snake_y += move_vertical
+        background(game_board)
+        snake(snake_size, snake_list)
 
-def random_position(apple):
-    """
-  Randomise the position of the apple on the game load.
-  """
-    apple.position = (random.randint(0, grid_width - 1) * grid_size,
-                      random.randint(0, grid_height - 1) * grid_size)
-
-class Apple():
-    def __init__(apple):
-
-        apple.position = (0, 0)
-        apple.color = (200, 0, 0)
-        apple.random_position()
-
-    def random_position(apple):
-        apple.position = (random.randint(0, grid_width - 1) * grid_size,
-                          random.randint(0, grid_height - 1) * grid_size)
-
-    def create_apple(apple, game_board):
-      """
-      Creates the apple using the pygame.rect, loads the apple in the given position via the variable apple_position and coloured in apple_colour. A further white square has been used for a border.
-      """
-      apple_square = pygame.Rect((apple.position[0], apple.position[1]),
-                                   (grid_size, grid_size))
-      pygame.draw.rect(game_board, apple.color, apple_square)
-      pygame.draw.rect(game_board, (white), apple_square, 1)
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    background(game_board)
-    snake(game_board)
-    apple = Apple()
-    apple.create_apple(game_board)
-
-    font_score = pygame.font.SysFont("Courier", 16)
-    text = font_score.render("Score {0}".format(snake_score), 1, (white))
-
-    screen.blit(game_board, (0, 0))
-    screen.blit(text, (5, 10))
-    pygame.display.update()
-
-  # Directions
-    exit_game = False
-    game_over = False   
-    while not exit_game:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_game = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                move_x = 20
-                move_y = 0
-
-            if event.key == pygame.K_LEFT:
-                move_x = - 20
-                move_y = 0
-
-            if event.key == pygame.K_UP:
-                move_y = - 20
-                move_x = 0
-
-            if event.key == pygame.K_DOWN:
-                move_y = 20
-                move_x = 0
-
-snake_x = snake_x + move_x
-snake_y = snake_y + move_y
-clock = pygame.time.Clock()
-clock.tick(fps)
-pygame.display.update()
+        pygame.draw.rect(game_board, apple_colour, [apple_x, apple_y, grid_size, grid_size])
+        pygame.draw.rect(game_board, white, [apple_x, apple_y, grid_size, grid_size],1)
+        snake_head = []
+        snake_head.append(snake_x)
+        snake_head.append(snake_y)
+        snake_list.append(snake_head)
+        if len(snake_list) > snake_length:
+            del snake_list[0]
 
 
+ 
+        snake_score(snake_length-1)
+ 
+        pygame.display.update()
+ 
+        clock.tick(snake_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+ 
+gameloop()
 
-
-
-
-
-    
-
-
-
-main()
